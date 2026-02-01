94430f0377ae9243d708aba99e2ea18527edd30c
son882005 <sont4634@gmail.com>
2026-02-01 22:09:22 +0700

    update home ui

--- mobile/lib/navigation/main_navigation.dart
+++ mobile/lib/navigation/main_navigation.dart
@@ -12,7 +12,12 @@ class MainNavigation extends StatefulWidget {
 class _MainNavigationState extends State<MainNavigation> {
   int _currentIndex = 0;
 
-  final List<Widget> _screens = const [HomeScreen(), ProfileScreen()];
+  final List<Widget> _screens = const [
+    HomeScreen(), // Home
+    Placeholder(), // Statistic (sau này thay)
+    Placeholder(), // Wallet (sau này thay)
+    ProfileScreen(), // Profile
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +25,7 @@ class _MainNavigationState extends State<MainNavigation> {
       body: _screens[_currentIndex],
 
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // QUAN TRỌNG để hiện 4 icon
         currentIndex: _currentIndex,
         onTap: (index) {
           setState(() {
@@ -27,8 +33,17 @@ class _MainNavigationState extends State<MainNavigation> {
           });
         },
         selectedItemColor: const Color(0xff2A7C76),
+        unselectedItemColor: Colors.grey,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: 'Statistic',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            label: 'Wallet',
+          ),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
         ],
       ),
