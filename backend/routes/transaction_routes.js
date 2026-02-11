59f680ab43e65791dc2afd5a9f9ae2f0b962be2e
Dyu20705 <duy28092005yh@gmail.com>
2026-02-11 12:01:09 +0700

    [Fix] Tối ưu định tuyến sử dụng middleware

--- backend/routes/transaction_routes.js
+++ backend/routes/transaction_routes.js
@@ -5,12 +5,19 @@
 
 const express = require("express");
 const router = express.Router();
+const { getTransactions } = require("../controllers/transaction_controller");
+const authenticate = require("../middleware/auth.middleware");
+const validate = require("../middleware/validate.middleware");
+const { getTransactionsSchema } = require("../validators/transaction.validator");
 
-const {
-  getTransactions,
-} = require("../controllers/transaction_controller");
+// // ✅ GET /api/transactions
+// router.get("/", getTransactions);
 
-// ✅ GET /api/transactions
-router.get("/", getTransactions);
+router.get(
+  "/",
+  authenticate,
+  validate(getTransactionsSchema, 'query'),
+  getTransactions
+);
 
 module.exports = router;
