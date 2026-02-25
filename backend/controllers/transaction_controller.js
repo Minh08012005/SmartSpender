bca229c32549a4634041fc8cc7a07a3621d9e84f
VuNgNgocBao04 <jackoldbao@gmail.com>
2026-02-26 02:24:41 +0700

    feat: implement create transaction controller

--- backend/controllers/transaction_controller.js
+++ backend/controllers/transaction_controller.js
@@ -22,3 +22,70 @@ exports.getTransactions = async (req, res, next) => {
     next(error);
   }
 };
+
+/**
+ * Create transaction
+ */
+exports.createTransaction = async (req, res, next) => {
+  try {
+    const transaction = await transactionService.createTransaction(
+      req.user._id,
+      req.body
+    );
+
+    return res.status(201).json(
+      successResponse(201, "Transaction created successfully", transaction)
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * Update transaction
+ */
+exports.updateTransaction = async (req, res, next) => {
+  try {
+    const updated = await transactionService.updateTransaction(
+      req.user._id,
+      req.params.id,
+      req.body
+    );
+
+    if (!updated) {
+      return res.status(404).json(
+        successResponse(404, "Transaction not found")
+      );
+    }
+
+    return res.status(200).json(
+      successResponse(200, "Transaction updated successfully", updated)
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * Delete transaction
+ */
+exports.deleteTransaction = async (req, res, next) => {
+  try {
+    const deleted = await transactionService.deleteTransaction(
+      req.user._id,
+      req.params.id
+    );
+
+    if (!deleted) {
+      return res.status(404).json(
+        successResponse(404, "Transaction not found")
+      );
+    }
+
+    return res.status(200).json(
+      successResponse(200, "Transaction deleted successfully")
+    );
+  } catch (error) {
+    next(error);
+  }
+};
